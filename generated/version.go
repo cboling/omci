d770b35b706baad23c32d5028541b7b4c5859dbd
Chip Boling <chip.boling@tibitcom.com>
2019-12-15 09:33:14 -0600

    Recent unknown CID message output introduced unwanted recursive call

--- generated/version.go
+++ generated/version.go
@@ -61,8 +61,8 @@ func init() {
 	Versions = append(Versions,
 		VersionInfo{
 			Name:       "code-generator",
-			Version:    "0.12.5",
-			CreateTime: 1576358874.714314,
+			Version:    "0.12.6",
+			CreateTime: 1576423858.6745758,
 			ItuDocName: "T-REC-G.988-2017-11.docx",
 			SHA256:     "96ffc8bca6f70175c8e281e87e1cf21662d07a7502ebf595c5c3180a9972b9ac",
 		})
